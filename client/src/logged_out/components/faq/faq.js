74ddbe131a87c042ed79ec020ca137468d0e88b7
aaziz <angelina.aziz@anaplan.com>
2023-03-11 12:12:51 +0000

    update faq section

--- client/src/logged_out/components/faq/faq.js
+++ client/src/logged_out/components/faq/faq.js
@@ -1,87 +1,113 @@
-import React, { useState } from "react";
-import classNames from "classnames";
-import { Box } from "@mui/material";
-import withStyles from "@mui/styles/withStyles";
-
-
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { Box, Grid } from '@mui/material';
+import withStyles from '@mui/styles/withStyles';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 const styles = (theme) => ({
   videoContentWrapper: {
-    backgroundColor: "#FEE64E",
+    backgroundColor: '#FEE64E',
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    [theme.breakpoints.up("sm")]: {
+    [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(4),
-      marginRight: theme.spacing(4),
+      marginRight: theme.spacing(4)
     },
     maxWidth: 1280,
-    width: "100%",
-    justifyContent: "center",
+    width: '100%',
+    justifyContent: 'center'
   },
   wrapper: {
-    minHeight: "60vh",
+    minHeight: '60vh'
   },
   noDecoration: {
-    textDecoration: "none !important",
-  },
+    textDecoration: 'none !important'
+  }
 });
 
 const FAQList = [
-    {
-      id: 1,
-      question: "How long does it take to get feedback?",
-      answer: "A maximum of 24 hours.",
-    },
-    {
-      id: 2,
-      question: "How does the service work?",
-      answer:
-        "You login, choose a subject and generate a question and upload a video of yourself speaking, we then use artificial intelligence to analyze your speech and provide you with feedback.",
-    },
-    {
-      id: 3,
-      question: "What is included in the feedback?",
-      answer:
-        "We provide you with a score out of 35, tell you how you rank against others, give a breakdown of your strengths and weaknesses, and a list of tips to improve your speaking.",
-    },
-    {
-      id: 4,
-      question: "Why are you called Auralyze.ai?",
-      answer:
-        "Auralyze means to analyze sound, and we use artificial intelligence to analyze your interviews and provide feedback. (we also think it sounds cool)",
-    },
-  ];
-  
-  function FAQ({ id, question, answer, activeId, setActiveId }) {
-    const handleClick = () => {
-      setActiveId(id === activeId ? null : id);
-    };
-  
-    return (
-      <div className={`faq ${id === activeId ? "active" : ""}`} onClick={handleClick}>
-        <div className="question"><b>{question}</b></div>
-        {id === activeId && <div className="answer">{answer}</div>}
-      </div>
-    );
+  {
+    id: 1,
+    question: 'How long does it take to get feedback?',
+    answer: 'A maximum of 24 hours.'
+  },
+  {
+    id: 2,
+    question: 'How does the service work?',
+    answer:
+      'You login, choose a subject and generate a question and upload a video of yourself speaking, we then use artificial intelligence to analyze your speech and provide you with feedback.'
+  },
+  {
+    id: 3,
+    question: 'What is included in the feedback?',
+    answer:
+      'We provide you with a score out of 35, tell you how you rank against others, give a breakdown of your strengths and weaknesses, and a list of tips to improve your speaking.'
+  },
+  {
+    id: 4,
+    question: 'Why are you called Auralyze.ai?',
+    answer:
+      'Auralyze means to analyze sound, and we use artificial intelligence to analyze your interviews and provide feedback. (we also think it sounds cool)'
   }
+];
+
+function FAQ({ id, question, answer, activeId, setActiveId }) {
+  return (
+    <Card
+      sx={{
+        width: '100%',
+        backgroundColor: id === activeId ? '#fff' : '#BFA4F8',
+        height: 'max-content',
+        marginTop: 3,
+        borderRadius: 4,
+        marginBottom: 2,
+      }}
+    >
+      <CardContent>
+        <Box display={'flex'} >
+          {id === activeId ? (
+            <RemoveIcon
+              onClick={() => setActiveId(null)}
+
+              sx={{ fontSize: 35,color:'red' }}
+            />
+          ) : (
+            <AddIcon onClick={() => setActiveId(id)} sx={{ fontSize: 40 }} />
+          )}
+          <Box>
+            <Typography fontSize={16} mx={1} mt={0.5} color='black' gutterBottom>
+              {question}
+            </Typography>
+            {id === activeId && (
+              <Typography variant='body2'>{answer}</Typography>
+            )}
+          </Box>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
 
 function Faq(props) {
-  const { classes, theme } = props;
+  const { classes } = props;
   const [activeId, setActiveId] = useState(null);
 
-
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      backgroundColor="#FEE64E"
-      className={classNames(classes.wrapper, "lg-p-top")}
+    <Grid
+      container
+      lg={12}
+      display='flex'
+      justifyContent='center'
+      backgroundColor='#FEE64E'
+      className={classNames(classes.wrapper, 'lg-p-top')}
     >
-      <div className={classes.videoContentWrapper}>
-      <div className="App">
-      <h1>Frequently Asked Questions</h1>
-      <div className="faq-list">
-        {FAQList.map((faq) => (
+      <Grid item lg={6} sm={12}>
+        <Typography sx={{fontWeight:'bold',fontSize:40,}} mb={1} textAlign='center' >Frequently Asked Questions<br /></Typography>
+        {FAQList?.map((faq) => (
           <FAQ
             key={faq.id}
             id={faq.id}
@@ -91,13 +117,9 @@ function Faq(props) {
             setActiveId={setActiveId}
           />
         ))}
-      </div>
-    </div>
-      </div>
-    </Box>
+      </Grid>
+    </Grid>
   );
 }
-
-
 
 export default withStyles(styles, { withTheme: true })(Faq);
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // Defines the mongodb data base to use.
 module.exports = {
-  mongoURI: process.env.MongoURI,
+  mongoURI: "mongodb+srv://angelinaaziz:QGvoxW6d9pa7im61@auralyze.wcfuyws.mongodb.net/test",
   secretOrKey: "secret",
   useUnifiedTopology: true
 };
